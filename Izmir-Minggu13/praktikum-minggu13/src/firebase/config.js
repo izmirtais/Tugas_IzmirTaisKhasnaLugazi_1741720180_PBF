11e8c3f7d20169bab295209ab3eef2ed1c5f07fc
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2020-05-01 00:50:09 +0700

    praktikum-minggu13

---
+++ Izmir-Minggu13/praktikum-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiRs_ov91O8lpgROOp_YowyDZeBjq-xqQ",
+    authDomain: "minggu-12.firebaseapp.com",
+    databaseURL: "https://minggu-12.firebaseio.com",
+    projectId: "minggu-12",
+    storageBucket: "minggu-12.appspot.com",
+    messagingSenderId: "208265740640",
+    appId: "1:208265740640:web:2576b536d198cc29e21e05",
+    measurementId: "G-8T5RFDF81H"
+  };
+
+export default firebaseConfig;
